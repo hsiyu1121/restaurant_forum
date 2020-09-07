3e957c00704fffc7ebe174f51d1ec68b6ff23dac
hsiyu1121 <bluefish1121@gmail.com>
2020-09-07 16:14:17 +0800

    Add restaurant index page

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
-app.engine("handlebars", handlebars());
+app.engine("handlebars", handlebars({defaultLayout:'main'}));
 app.set("view engine", "handlebars");
 
 app.listen(port, () => {
   console.log(`Express server listening on http://localhost:${port}`);
 });
+
+require('./routes')(app)
---
+++ controllers/restController.js
@@ -0,0 +1,8 @@
+const restController = {
+  getRestaurants: (req, res) => {
+    return res.render('restaurants')
+  } 
+}
+
+
+module.exports = restController
