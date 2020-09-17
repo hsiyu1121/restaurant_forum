c08ad81f5d78f76536d875241d9cbb1b01ac99e3
hsiyu1121 <bluefish1121@gmail.com>
2020-09-17 10:29:33 +0800

    Add dashboard page

--- controllers/restController.js
+++ controllers/restController.js
@@ -81,8 +81,19 @@ const restController = {
         })
       })
     })
+  },
+
+  getDashboard: (req, res) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: [
+        Category, 
+        {model: Comment, include: [User]}
+      ]
+    }).then(restaurant => {
+      return res.render('dashboard', {restaurant: restaurant.toJSON()})
+    })
   }
- 
+
 
 }
 
