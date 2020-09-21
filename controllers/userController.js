bf7d225542daefed3ba0ed103d29b342aee28ee2
hsiyu1121 <bluefish1121@gmail.com>
2020-09-21 14:29:36 +0800

    update user profile debug

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,62 +1,19 @@
-const bcrypt = require('bcryptjs')
-const db = require('../models')
-const User = db.User
-const Comment = db.Comment
-const Restaurant = db.Restaurant
-const Favorite = db.Favorite
-const Like = db.Like
-const Followship = db.Followship
-const fs = require('fs')
-const imgur = require('imgur-node-api')
-const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+const bcrypt = require("bcryptjs");
+const db = require("../models");
+const User = db.User;
+const Comment = db.Comment;
+const Restaurant = db.Restaurant;
+const Favorite = db.Favorite;
+const Like = db.Like;
+const Followship = db.Followship;
+const fs = require("fs");
+const imgur = require("imgur-node-api");
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID;
 
 const userController = {
-  signUpPage: (req, res) => {
-    return res.render('signup')
-  },
-
-  signUp: (req, res) => {
-    if(req.body.passwordCheck !== req.body.password){
-      req.flash('error_messages', '兩次密碼輸入不同 ！')
-      return res.redirect('/signup')
-    } else {
-      User.findOne({where: {email: req.body.email}})
-        .then(user => {
-          if(user){
-            req.flash('error_messages', '此Email已註冊 ！')
-            return res.redirect('/signup')
-          } else {
-            User.create({
-              name: req.body.name, 
-              email: req.body.email, 
-              password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-            }).then(user => {
-              req.flash('success_messages', '成功註冊帳號 ！')
-              return res.redirect('/signin')
-            })
-          }
-        })
-    }    
-  }, 
-
-  signInPage: (req, res) => {
-    return res.render('signin')
-  },
-
-  signIn: (req, res) => {
-    req.flash('success_messages', '成功登入')
-    res.redirect('/restaurants')
-  },
-
-  logout: (req, res) => {
-    req.flash('success_messages', '登出成功')
-    req.logout()
-    res.redirect('/signin')
-  },
-
-  getUser: (req, res) => {
-    const userSelf = req.user.id
-    return User.findByPk(req.user.id, {
+  
+  getUser: (req, res) => {   
+    User.findByPk(req.params.id, {
       include: [
         {model: Comment, include:[Restaurant]},
         {model: Restaurant, as: 'FavoritedRestaurants'}, 
@@ -65,6 +22,7 @@ const userController = {
       ]
       })
       .then(user => {
+        const userSelf = req.user.id
         const isFollowed = req.user.Followings.map(d => d.id).includes(user.id)
         return res.render('users', 
         {user:user.toJSON(), 
@@ -74,132 +32,172 @@ const userController = {
   },
 
   editUser: (req, res) => {
-    return User.findByPk(req.params.id)
-      .then(user => {
-        return res.render('edit')
-      })
+    return User.findByPk(req.params.id).then((user) => {
+      return res.render("edit");
+    });
   },
 
   putUser: (req, res) => {
     if (!req.body.name) {
-      req.flash('error_messages', "name didn't exist")
-      return res.redirect('back')
+      req.flash("error_messages", "name didn't exist");
+      return res.redirect("back");
     }
 
-    const { file } = req
-    if(file){
-      imgur.setClientID(IMGUR_CLIENT_ID)
+    const { file } = req;
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID);
       imgur.upload(file.path, (err, img) => {
-        return User.findByPk(req.params.id)
-          .then(user => {
-            user.update({
-              name: req.body.name, 
-              image: file ? img.data.link : null
-          }).then(user => {
-            req.flash('success_messages', 'user was successfully to update')
-            res.redirect(`/users/${user.id}`)
-          })
-        })
-      })
+        return User.findByPk(req.params.id).then((user) => {
+          user
+            .update({
+              name: req.body.name,
+              image: file ? img.data.link : null,
+            })
+            .then((user) => {
+              req.flash("success_messages", "user was successfully to update");
+              res.redirect(`/users/${user.id}`);
+            });
+        });
+      });
     } else {
-      return User.findByPk(req.params.id)
-        .then(user => {
-          user.update({
-            name: req.body.name, 
-            image: user.image
-        }).then(user => {
-          req.flash('success_messages', 'user was successfully to update')
-          res.redirect(`/users/${user.id}`)
-        })
-      })
+      return User.findByPk(req.params.id).then((user) => {
+        user.update({
+            name: req.body.name,
+            image: user.image,
+          })
+          .then((user) => {
+            req.flash("success_messages", "user was successfully to update");
+            res.redirect(`/users/${user.id}`);
+          });
+      });
     }
   },
 
   addFavorite: (req, res) => {
     return Favorite.create({
-      UserId: req.user.id, 
-      RestaurantId: req.params.restaurantId
-    })
-      .then(restaurant => {
-        return res.redirect('back')
-      })
-  }, 
-  
+      UserId: req.user.id,
+      RestaurantId: req.params.restaurantId,
+    }).then((restaurant) => {
+      return res.redirect("back");
+    });
+  },
+
   removeFavorite: (req, res) => {
-    return Favorite.findOne({ where: {
-      UserId: req.user.id, 
-      RestaurantId: req.params.restaurantId
-    }})
-      .then(favorite => {
-        favorite.destroy()
-          .then(restaurant => {
-            return res.redirect('back')
-          })
-      })
-  }, 
+    return Favorite.findOne({
+      where: {
+        UserId: req.user.id,
+        RestaurantId: req.params.restaurantId,
+      },
+    }).then((favorite) => {
+      favorite.destroy().then((restaurant) => {
+        return res.redirect("back");
+      });
+    });
+  },
 
   addLike: (req, res) => {
     return Like.create({
-      UserId: req.user.id, 
-      RestaurantId: req.params.restaurantId
-    })
-      .then(restaurant => {
-        return res.redirect('back')
-      })
-  }, 
+      UserId: req.user.id,
+      RestaurantId: req.params.restaurantId,
+    }).then((restaurant) => {
+      return res.redirect("back");
+    });
+  },
 
   removeLike: (req, res) => {
-    return Like.findOne({ where: {
-      UserId: req.user.id, 
-      RestaurantId: req.params.restaurantId
-    }})
-      .then(like => {
-        like.destroy()
-          .then(restaurant => {
-            return res.redirect('back')
-          })
-      })
+    return Like.findOne({
+      where: {
+        UserId: req.user.id,
+        RestaurantId: req.params.restaurantId,
+      },
+    }).then((like) => {
+      like.destroy().then((restaurant) => {
+        return res.redirect("back");
+      });
+    });
   },
 
   getTopUser: (req, res) => {
     return User.findAll({
-      include: [
-        {model: User, as: 'Followers'}
-      ]
-    }).then(users => {
-      users = users.map(user => ({
-        ...user.dataValues, 
+      include: [{ model: User, as: "Followers" }],
+    }).then((users) => {
+      users = users.map((user) => ({
+        ...user.dataValues,
         FollowerCount: user.Followers.length,
-        isFollowed: req.user.Followings.map(d => d.id).includes(user.id)
-      }))
-      users = users.sort((a, b) => b.FollowerCount - a.FollowerCount)
-      return res.render('topUser', {users: users})
-    })
+        isFollowed: req.user.Followings.map((d) => d.id).includes(user.id),
+      }));
+      users = users.sort((a, b) => b.FollowerCount - a.FollowerCount);
+      return res.render("topUser", { users: users });
+    });
   },
 
   addFollowing: (req, res) => {
     return Followship.create({
-      followerId: req.user.id, 
-      followingId: req.params.userId
-    })
-    .then((followship)  => {
-      return res.redirect('back')
-    })
+      followerId: req.user.id,
+      followingId: req.params.userId,
+    }).then((followship) => {
+      return res.redirect("back");
+    });
   },
 
   removeFollowing: (req, res) => {
-    return Followship.findOne({where: {
-      followerId: req.user.id,
-      followingId: req.params.userId
-    }})
-    .then((followship) => {
-      followship.destroy()
-        .then((followship) => {
-          return res.redirect('back')
-        })
-    })
-  }
-    
-}
+    return Followship.findOne({
+      where: {
+        followerId: req.user.id,
+        followingId: req.params.userId,
+      },
+    }).then((followship) => {
+      followship.destroy().then((followship) => {
+        return res.redirect("back");
+      });
+    });
+  },
 
-module.exports = userController
+  signUpPage: (req, res) => {
+    return res.render("signup");
+  },
+
+  signUp: (req, res) => {
+    if (req.body.passwordCheck !== req.body.password) {
+      req.flash("error_messages", "兩次密碼輸入不同 ！");
+      return res.redirect("/signup");
+    } else {
+      User.findOne({ where: { email: req.body.email } }).then((user) => {
+        if (user) {
+          req.flash("error_messages", "此Email已註冊 ！");
+          return res.redirect("/signup");
+        } else {
+          User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: bcrypt.hashSync(
+              req.body.password,
+              bcrypt.genSaltSync(10),
+              null
+            ),
+          }).then((user) => {
+            req.flash("success_messages", "成功註冊帳號 ！");
+            return res.redirect("/signin");
+          });
+        }
+      });
+    }
+  },
+
+  signInPage: (req, res) => {
+    return res.render("signin");
+  },
+
+  signIn: (req, res) => {
+    req.flash("success_messages", "成功登入");
+    res.redirect("/restaurants");
+  },
+
+  logout: (req, res) => {
+    req.flash("success_messages", "登出成功");
+    req.logout();
+    res.redirect("/signin");
+  }
+};
+
+module.exports = userController;
