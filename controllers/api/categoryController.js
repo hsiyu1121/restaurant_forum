f28fdb813a5ad248fd0b90a283d7e37969f9e7ee
hsiyu1121 <bluefish1121@gmail.com>
2020-09-24 10:42:52 +0800

    DELETE /api/admin/categories/:id

--- controllers/api/categoryController.js
+++ controllers/api/categoryController.js
@@ -21,6 +21,13 @@ let categoryController = {
       return res.json(data)
     })
   },
+
+  deleteCategory: (req, res) => {
+    categoryService.deleteCategory(req, res, (data) => {
+      return res.json(data)
+    })
+  }
+
 }
 
 module.exports = categoryController 
