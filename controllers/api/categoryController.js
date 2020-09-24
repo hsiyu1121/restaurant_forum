e761fc68da9c4f120e4e6d7d60aacff572a1b22c
hsiyu1121 <bluefish1121@gmail.com>
2020-09-24 10:00:45 +0800

    POST /api/admin/categories

--- controllers/api/categoryController.js
+++ controllers/api/categoryController.js
@@ -8,6 +8,12 @@ let categoryController = {
     categoryService.getCategories(req, res, (data) => {
       return res.json(data)
     })
+  },
+
+  postCategory:(req, res) => {
+    categoryService.getCategories(req, res, (data) => {
+      return res.json(data)
+    })
   }
 }
 
--- services/categoryService.js
+++ services/categoryService.js
@@ -14,6 +14,20 @@ let categoryService = {
 
       }
     })
+  },
+
+  postCategory:(req, res, callback) => {
+  if (!req.body.name) {
+    return callback({status:'error', message: "name didn't exist"})
+  } else {
+    return Category.create({
+      name: req.body.name
+    })
+    .then(category => {
+      callback({status:'success', message:'category was successfully created'})
+    })
   }
+}
+
 }
 module.exports = categoryService 
